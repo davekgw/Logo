aa1b08582c3d8cf1c019f03c09add6946911f8ad
DaveKgw1 <45153558+davekgw@users.noreply.github.com>
2019-06-19 00:19:08 +0800

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -1,30 +1,19 @@
 #!usr/bin/bash
-
-#colour
-R='\033[1;31m'
-B='\033[0;34m'
-C='\033[0;36m'
-G='\033[1;32m'
-W='\033[1;37m'
-Y='\033[0;33m'
-Y2='\033[1;33m'
-
 #variables
 dir=/data/data/com.termux/files/home
-E=/data/data/com.termux/files/usr/share/Log/logo
+E=/data/data/com.termux/files/usr/share/Logo/logo
 
-clear
-echo
-echo $R"       ████████╗   ██████   ██████    ██     "
-echo $B"       ╚══██╔══╝  ██    ██ ██    ██   ██     "
-echo $C"          ██║     ██    ██ ██    ██   ██      "
-echo $Y"          ██║     ██    ██ ██    ██   ██     "
-echo $W"          ██║      ██████   ██████    ███████ $R v1"
+echo ""
+echo "       ████████╗   ██████   ██████    ██     "
+echo "       ╚══██╔══╝  ██    ██ ██    ██   ██     "
+echo "          ██║     ██    ██ ██    ██   ██      "
+echo "          ██║     ██    ██ ██    ██   ██     "
+echo "          ██║      ██████   ██████    ███████ $R v1"
 echo "          ╚═╝                     "
-echo $R"[$G"01"$R]$C"-"$R["$W"Termux$C"-"$W"Theme"$R]"
-echo $R"[$G"BB"$R]$B"-"$R["$W"BaCk$R]"
-echo "               "
-echo -n $W"Theme:~$R"#" " 
+echo "[01]-[Termux-Theme]"
+echo "[BB]-[BaCk]"
+echo ""
+echo -n "Theme:~#" " 
 read zaki
 #Logo
 if [ "$zaki" = "01" ] || [ "$zaki" = "1" ];
@@ -58,4 +47,4 @@ figlet -f big "   $logo" | lolcat
 echo " Done"
 rm -rf $dir/logo
 sleep 1
-$PREFIX/share/Log/logo.sh
+$PREFIX/share/Logo/logo.sh
